3a34cae3690c968fe6f0288819aeb3101b27405e
jared <jaredkjar@gmail.com>
2024-09-09 19:08:52 -0600

    first commit

---
+++ src/App.svelte
@@ -0,0 +1,18 @@
+<script>
+  export let name;
+</script>
+
+<img
+  src="https://static-00.iconduck.com/assets.00/svelte-icon-426x512-cm99sfkq.png"
+  alt="svelte-icon"
+  height="200px"
+/>
+<h1>You are on the svelte front end!</h1>
+
+<style>
+  img {
+    display: block;
+    margin-left: auto;
+    margin-right: auto;
+  }
+</style>
