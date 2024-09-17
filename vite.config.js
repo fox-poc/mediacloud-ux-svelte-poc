453bc5f5c696587d347b12823817cb82429e06d8
jared <jaredkjar@gmail.com>
2024-09-17 15:02:36 -0600

    build

--- vite.config.js
+++ vite.config.js
@@ -2,15 +2,14 @@
 import { defineConfig } from "vite";
 import { svelte } from "@sveltejs/vite-plugin-svelte";
 import vitePluginSingleSpa from "vite-plugin-single-spa";
-
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: "./",
-  plugins: [
-    svelte(),
-    vitePluginSingleSpa({
-      serverPort: 3004,
-      spaEntryPoints: "src/spa.tsx",
-    }),
-  ],
+    base: "./",
+    plugins: [
+        svelte(),
+        vitePluginSingleSpa({
+            serverPort: 3004,
+            spaEntryPoints: "src/spa.tsx",
+        }),
+    ],
 });
