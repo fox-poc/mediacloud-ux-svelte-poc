56ab58634439a97fdbcc5ea3d84e8840f7839824
jared <jaredkjar@gmail.com>
2024-09-19 16:33:42 -0600

    fixes

--- src/App.svelte
+++ src/App.svelte
@@ -1,15 +1,19 @@
+<script>
+  // groups and token are being passed in from the root config
+  export let groups;
+  export let token;
+  let fakeChannels = [
+    { title: "fake channel", url: "none" },
+    { title: "fake channel 2", url: "none" },
+  ];
+  console.log("groups", groups, "token,", token);
+</script>
+
 <img
   src="https://static-00.iconduck.com/assets.00/svelte-icon-426x512-cm99sfkq.png"
   alt="svelte-icon"
   height="150px"
 />
-
-<script>
-  // groups and token are being passed in from the root config
-  export let groups;
-  export let token;
-  console.log('groups', groups, 'token,', token);
-</script>
 
 <br />
 <br />
@@ -21,9 +25,4 @@
   This component is being passed data that the styleguide will map out and
   style.
 </h3>
-<collection-cards
-  items={JSON.stringify([
-    { title: "fake channel", url: "none" },
-    { title: "fake channel 2", url: "none" },
-  ])}
-/>
+<collection-cards items={fakeChannels} />
